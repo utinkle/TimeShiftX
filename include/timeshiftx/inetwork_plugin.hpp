9522da57415c92687417eb1dfc6932d7cfa6b70c
Chen Bin <chenbin@purecompute.cn>
2026-04-27 10:38:37 +0800

    修复编译问题

--- include/timeshiftx/inetwork_plugin.hpp
+++ include/timeshiftx/inetwork_plugin.hpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <cstdint>
 #include <string>
+#include <thread>           
+#include <atomic>
 
 #include "timeshiftx/network_types.hpp"
 
@@ -15,21 +17,26 @@ public:
 
     virtual std::string name() const = 0;
     virtual NetworkResponse perform(const NetworkRequest& request) = 0;
+
     virtual void performAsync(const NetworkRequest& request, NetworkCallback callback) {
-        std::async(std::launch::async, [this, request, callback = std::move(callback)]() mutable {
+        std::thread([this, request, callback = std::move(callback)]() mutable {
             callback(perform(request));
-        });
+        }).detach();
     }
+
     virtual std::uint64_t performAsyncTracked(const NetworkRequest& request, NetworkCallback callback) {
+        static std::atomic<std::uint64_t> nextId{1};
+        std::uint64_t id = nextId.fetch_add(1);
         performAsync(request, std::move(callback));
-        return 0;
+        return id;
     }
+
     virtual bool cancel(std::uint64_t) { return false; }
 
     virtual std::future<NetworkResponse> performAsyncFuture(const NetworkRequest& request) {
         auto promise = std::make_shared<std::promise<NetworkResponse>>();
         std::future<NetworkResponse> fut = promise->get_future();
-        performAsync(request, [promise](NetworkResponse resp) {
+        performAsync(request, [promise](NetworkResponse resp) mutable {
             promise->set_value(std::move(resp));
         });
         return fut;
